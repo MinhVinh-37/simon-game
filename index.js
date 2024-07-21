6ea32db4535934fefa06ec8fda341a96f5a50d42
MinhVinh-37 <nguyenminhvinh37@gmail.com>
2024-07-21 18:06:10 +0700

    Add function

---
+++ index.js
@@ -0,0 +1,85 @@
+var lightedButtons = [];
+var pressedButtons = [];
+var level = 1;
+const colorValue = {
+  green: 0,
+  red: 1,
+  yellow: 2,
+  blue: 3,
+};
+const reverseColorValue = {
+  0: "green",
+  1: "red",
+  2: "yellow",
+  3: "blue",
+};
+function activeGame() {
+  $("body").on("keypress", () => {
+    // Start the game!
+    $(".title").text("Level 1");
+    $("body").off();
+    lightButton();
+    activePressButton();
+  });
+}
+
+function lightButton() {
+  var randomColorValue = Math.floor(Math.random() * 4);
+  color = reverseColorValue[randomColorValue];
+  lightedButtons.push(color);
+  $("#" + color).addClass("light");
+  setTimeout(() => {
+    $("#" + color).removeClass("light");
+  }, 500);
+}
+
+function activePressButton() {
+  $(".button").on("click", (event) => {
+    pressedColor = event.currentTarget.id;
+    $("#" + pressedColor).addClass("pressed");
+    setTimeout(() => {
+      $("#" + pressedColor).removeClass("pressed");
+    }, 500);
+    new Audio("./sounds/" + pressedColor + ".mp3").play();
+    pressedButtons.push(pressedColor);
+    var indexCompare = pressedButtons.length - 1;
+    if (pressedButtons[indexCompare] == lightedButtons[indexCompare]) {
+      if (indexCompare == lightedButtons.length - 1) {
+        nextLevel();
+      }
+    } else {
+      youLose();
+    }
+  });
+}
+
+function nextLevel() {
+  pressedButtons = [];
+  level += 1;
+  $(".title").text("Level " + level);
+  lightButton();
+}
+
+function youLose() {
+  showErrorScreen();
+  resetParams();
+}
+
+function showErrorScreen() {
+  $(".title").text("Wrong bro, press any key to restart!");
+  $("body").addClass("lose");
+  setTimeout(() => {
+    $("body").removeClass("lose");
+  }, 500);
+  new Audio("./sounds/wrong.mp3").play();
+}
+
+function resetParams() {
+  $(".button").off();
+  lightedButtons = [];
+  pressedButtons = [];
+  level = 1;
+  setTimeout(activeGame, 1000);
+}
+
+activeGame();
